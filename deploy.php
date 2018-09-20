bd1603900435487534505f6d30decdfcb5e62510
Martin Janser <martin@duss-janser.ch>
2018-09-20 22:27:49 +0200

    Fix deployment and favicon

--- deploy.php
+++ deploy.php
@@ -2,21 +2,40 @@
 
 namespace Deployer;
 
-require 'recipe/symfony.php';
+require 'recipe/common.php';
 
 set('application', 'sgtuggen');
 set('repository', 'git@github.com:mjanser/sg-tuggen-website.git');
 set('git_tty', true);
+set('http_user', 'goganch');
 add('shared_files', ['public/.htaccess', '.env']);
 add('shared_dirs', []);
-add('writable_dirs', []);
+set('writable_dirs', ['var']);
 
 host('gogan.ch')
     ->user('goganch')
     ->set('deploy_path', '~/www/sgtuggen.gogan.ch');
 
-task('build', function () {
-    run('cd {{release_path}} && build');
+desc('Clear cache');
+task('deploy:cache:clear', function () {
+    run('{{bin/php}} {{release_path}}/bin/console cache:clear');
 });
 
+desc('Deploy project');
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:vendors',
+    'deploy:writable',
+    'deploy:cache:clear',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+]);
+
+after('deploy', 'success');
 after('deploy:failed', 'deploy:unlock');
